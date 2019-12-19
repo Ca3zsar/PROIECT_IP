72a9155787b50846c4625945fd558f4fac0140ab
Cezar Todirisca <cezartodirisca@gmail.com>
2019-12-19 12:35:35 +0200

    Update main_file.cpp

--- main_file.cpp
+++ main_file.cpp
@@ -4,6 +4,7 @@
 #include <vector> //Used for keeping expression after breaking text into words
 #include <sstream> //Included for stringstream
 #include <map> //Used for creating a "dictionary"
+#include <algorithm> //For importing for_each
 using namespace std;
 
 // Authors: Dobrovici Cristian && Todirisca Cezar
@@ -17,17 +18,54 @@ using namespace std;
 long long evaluate(),term(),factor(); //The functions used for the evaluation of the expression
 
 map<string,string>Operatie;
+map<string,int>Numar;
+string lang="ro";
+string fisier1 = "operatii_"+lang+".txt";
+string fisier2 = "modify_"+lang+".txt";
+string fisier3 = "logs_"+lang+".txt";
+string fisier4 = "template_numere.txt";
+string fisier5 = "template_cifre.txt";
 
-void MapTheOperations()
+string input_question;
+
+// Simulate a dictionary, where each word has a sign attributed
+void MapTheFiles()
 {
-    ifstream f1("operatii_ro.txt");
-    ifstream f2("modify_ro.txt");
+    ifstream f1(fisier1);
+    ifstream f2(fisier2);
 
     string cuvant,semn;
+    int num;
 
     while(getline(f1,cuvant) && getline(f2,semn))
     {
         Operatie[cuvant]=semn;
+    }
+
+    f1.close();
+    f2.close();
+
+    ifstream f1(fisier4);
+    ifstream f2(fisier5);
+
+    while(getline(f1,cuvant) && f2>>num)
+    {
+        Numar[cuvant]=num;
+    }
+
+    f1.close();
+    f2.close();
+}
+
+void log_print(int wrong = 0, string Question,string answer)
+{
+    ofstream fout(fisier3);
+    fout<<Question<<'\n';
+    if(wrong==1)
+    {
+        fout<<">> Input invalid\n";
+    }else{
+        fout<<">>"<<answer<<'\n'; 
     }
 }
 
@@ -73,12 +111,40 @@ vector<string> split_expression(string expression)
             }
         }
     }
-    cout<<list_of_words.size()<<'\n';
-    for(auto vec:list_of_words)
-    {
-        cout<<vec<<'\n';
-    }
+
     return list_of_words;
+}
+
+bool word_in_file(string word)
+{
+    ifstream reader(fisier1);
+    string temp;
+    while(getline(reader,temp))
+    {
+        if(temp==word)return true;
+    }
+    reader.close();
+    return false;
+}
+
+bool is_word(string word)
+{
+    int size_of_word = word.size();
+    for(int i=0;i<size_of_word;i++)
+    {
+        if(!(word[i]>='a' && word[i]<='z'))return false;
+    }
+    return true;
+}
+
+bool is_number(string word)
+{
+    int size_of_word = word.size();
+    for(int i=0;i<size_of_word;i++)
+    {
+        if(!(word[i]>='0' && word[i]<='9'))return false;
+    }
+    return true;
 }
 
 /*Select the correct words from the previous processed list
@@ -88,6 +154,64 @@ template from the text file*/
 vector<string> select_correct_words(vector<string> list_of_words)
 {
     vector<string>correct_words;
+    int size_of_list = list_of_words.size();
+    string word;
+    string simboluri = "()+-*/%";
+
+    bool apare_dintre = (find(list_of_words.begin(),list_of_words.end(),"dintre")!=list_of_words.end());
+    //We need this to check for the"si" between the numbers
+
+    //In cazul in care unele cuvinte contin o litera mare vor fi aduse la lowercase pentru
+    // a se potrivi cu "dictionarul"
+    for(int i=0; i<size_of_list; i++)
+    {
+        word = list_of_words[i];
+        for_each(word.begin(),word.end(), [](char &c){
+            c = tolower(c);
+        });
+        list_of_words[i]=word;
+    }
+
+    if(list_of_words[0]=="cat")
+    {
+        list_of_words.erase(list_of_words.begin());
+    }
+
+    for(int i=0; i<size_of_list; i++)
+    {
+        word = list_of_words[i];
+        if(word.size()==1)
+        {
+            if(simboluri.find(word) != string::npos)
+            {
+                correct_words.push_back(word);
+            }
+            else{
+                if(isdigit(word[0]))
+                {
+                    correct_words.push_back(word);
+                }
+            }
+        }else{
+            if(is_word(word))
+            {
+                if(word_in_file(word))
+                {
+                    correct_words.push_back(Operatie[word]);
+                }else{
+
+                }
+            }else{
+                if(is_number(word))
+                {
+                    correct_words.push_back(word);
+                }else{
+                    log_print(1,input_question,"");
+                }
+            }
+        }
+    }
+
     return correct_words;
 }
 
@@ -130,16 +254,18 @@ long long unsigned get_result_of_expression(string math_expression)
 /*Takes the input from the button in the menu and changes the text files
 according to the language chosen by the user. From now on the expression must be
 written in the chosen language or otwherwise it will be considered invalid*/
-void choose_language()
+void choose_language(int choice)
 {
-
+    if(choice==1)lang="ro";
+    if(choice==2)lang="en";
+    if(choice==3)lang="fr";
+    fisier1 = "operatii_"+lang+".txt";
+    fisier2 = "modify_"+lang+".txt";
+    fisier3 = "logs_"+lang+".txt";
 }
 
 int main()
 {
-    MapTheOperations();
-    string Expression;
-    getline(cin,Expression);
-    split_expression(Expression);
+    MapTheFiles();
     return 0;
 }
